6f2af7ef8dec9a29b62de3ae4318ffd482e3fc65
nuclear2025 <dmitrby@sce.ac.il>
2025-05-25 08:12:16 +0000

    Deploying to gh-pages from @ nuclear2025/nuclear2025.github.io@f92f7c6cf8b8c219696186a118deb80988ca9b4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/participants/";
+            window.location.href = "/organizers/";
           },
         },{id: "post-a-post-with-plotly-js",
         
